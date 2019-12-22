8260228ea2703e6de0d9ba93adcfb1019d012df3
suishidan <47165457+suishidan@users.noreply.github.com>
2019-12-22 22:00:37 +0900

    Add files via upload
    index

---
+++ Public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Fuzhou Yonghua market</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <h1>Fuzhou Yonghua market</h1>
+
+    <ul>
+      <li>
+        <a href="create.php"><strong>Create</strong></a> - add a member
+      </li>
+      <li>
+        <a href="read.php"><strong>Read</strong></a> - find a member
+      </li>
+    </ul>
+  </body>
+</html>
